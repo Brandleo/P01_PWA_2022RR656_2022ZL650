12e0d7279d96d94efbf600880b9a77847a1a3c9c
Brandleo <brandon.repreza@catolica.edu.sv>
2025-02-24 17:50:17 -0600

    Agregar archivos de proyecto.

---
+++ P01_2022RR656_2022ZL650/Models/BaseParqueoContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+
+namespace P01_2022RR656_2022ZL650.Models
+{
+    public class BaseParqueoContext: DbContext
+    {
+        public BaseParqueoContext(DbContextOptions<BaseParqueoContext> options) : base(options)
+
+
+
+        {
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
