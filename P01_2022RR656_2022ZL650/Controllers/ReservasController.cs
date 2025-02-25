bb06b288a83ea892a2b173996c8ab021164063b4
Brandleo <brandon.repreza@catolica.edu.sv>
2025-02-24 20:21:19 -0600

    Controllador reservas

---
+++ P01_2022RR656_2022ZL650/Controllers/ReservasController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P01_2022RR656_2022ZL650.Models;
+
+namespace P01_2022RR656_2022ZL650.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservasController : ControllerBase
+    {
+
+
+        public readonly BaseParqueoContext _parqueoContexto;
+
+
+
+        public ReservasController(BaseParqueoContext parqueoContext)
+        {
+            _parqueoContexto = parqueoContext;
+
+
+        }
+
+
+
+
+
+
+        [HttpPost]
+        [Route("Reservar/{usuarioId}")]
+        public IActionResult Reservar(int usuarioId, [FromBody] Reservas reserva)
+        {
+           
+            var usuario = (from u in _parqueoContexto.Usuarios
+                           join r in _parqueoContexto.Reservas on u.UsuarioID equals usuarioId
+                           where u.UsuarioID == usuarioId && u.Rol == "Cliente"
+                           select u).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return Unauthorized("El usuario no está autorizado para hacer reservas.");
+            }
+
+            
+            reserva.UsuarioID = usuarioId;
+
+            
+            _parqueoContexto.Reservas.Add(reserva);
+            _parqueoContexto.SaveChanges();
+
+            return Ok(reserva);
+        }
+
+
+
+
+
+
+
+    }
+}
