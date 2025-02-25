30c031c2a7d20a94529083ec456a455108ad8bb0
Brandleo <brandon.repreza@catolica.edu.sv>
2025-02-24 18:11:31 -0600

    DBsets creados

--- P01_2022RR656_2022ZL650/Models/BaseParqueoContext.cs
+++ P01_2022RR656_2022ZL650/Models/BaseParqueoContext.cs
@@ -17,7 +17,13 @@ namespace P01_2022RR656_2022ZL650.Models
         }
 
 
+        public DbSet<Usuarios> Usuarios { get; set; }
+        
+        public DbSet<Reservas> Reservas { get; set; }
 
+        public DbSet<EspaciosParqueo> EspaciosParqueos { get; set; }
+
+        public DbSet<Sucursales> Sucursales { get; set; }
 
 
 
